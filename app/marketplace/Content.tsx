6a1114f046667723460a764f4749440aa940b3d3
Christopher Lam <127339027+clam365@users.noreply.github.com>
2025-01-18 13:35:27 -0500

    aaa

--- app/marketplace/Content.tsx
+++ app/marketplace/Content.tsx
@@ -1,10 +1,85 @@
+import {Armchair, BookText, NotebookPen, Plug, Search, Shirt, ShoppingBasket, Store, Tag, Wrench} from "lucide-react";
+
 export default function Content() {
     return(
         <div className={"flex"}>
-            <aside className={"w-1/4"}>
-                yo
+            <aside className={"p-4 w-1/4 h-screen shadow-[4px_0_6px_-1px_rgba(0,0,0,0.1)]"}>
+                {/*Searchbar*/}
+                <div
+                    className="my-3 mb-6 hidden lg:flex items-center bg-[#f1f1f1] rounded-full px-4 py-2 w-full max-w-xl hover:bg-gray-200 transition">
+                    <Search className="text-gray-500 w-5 h-5"/>
+                    <input
+                        type="text"
+                        placeholder="Search spots"
+                        className="bg-transparent ml-2 w-full outline-none text-gray-700"
+                    />
+                </div>
+
+                {/* Options */}
+                <div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={" ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <ShoppingBasket className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Buying</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 mb-3 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Tag className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Selling</h1>
+                    </div>
+                </div>
+                <hr className={"mb-3"}/>
+                {/* Categories */}
+                <div className={""}>
+                    <h1 className={"font-semibold mb-3"}>Categories</h1>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Store className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Browse All Items</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={" ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <BookText className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Textbooks</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={" ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Wrench className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Tools/Equipment</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Armchair className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Furniture</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Plug className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Electronics</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <Shirt className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>Clothing</h1>
+                    </div>
+                    <div className={"flex gap-x-3 py-2 items-center hover:bg-gray-100 transition"}>
+                        <div className={"ml-2 bg-[#f1f1f1] hover:bg-gray-200 transition p-2 rounded-full"}>
+                            <NotebookPen className={"w-5 h-5 "}/>
+                        </div>
+                        <h1 className={"font-semibold"}>School Supplies</h1>
+                    </div>
+                </div>
+
             </aside>
-            <div className={"grid grid-cols-4"}>
+            <div className={"grid grid-cols-4 h-full"}>
 
             </div>
         </div>
--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
-import {CircleUserRound, MessageCircle} from "lucide-react";
+import { CircleUserRound, DiamondPlus, MessageCircle } from "lucide-react";
 import Content from "@/app/marketplace/Content";
 
 export default function page() {
     return (
         <>
-            <div className={"p-5 flex justify-between items-center"}>
-                <Image src={"/techtrade.png"} alt={"techtrade"} width={45} height={45}/>
+            <div className={"p-3 flex justify-between items-center drop-shadow-md bg-white"}>
+                <div className="flex items-center gap-2">
+                    <Image src={"/techtrade.png"} alt={"techtrade"} width={45} height={45} className={""} />
+                    <div className="text-2xl font-bold flex items-center">
+                        <span className="text-red-600">Tech</span>
+                        <span className="text-black">Trade</span>
+                    </div>
+                </div>
                 <div className={"gap-x-3 flex"}>
-                    <MessageCircle className={"w-10 h-10"}/>
-                    <CircleUserRound className={"w-10 h-10"}/>
+                    <DiamondPlus className={"w-9 h-9"} />
+                    <MessageCircle className={"w-9 h-9"} />
+                    <CircleUserRound className={"w-9 h-9"} />
                 </div>
             </div>
-            <Content/>
+            <Content />
         </>
-
-    )
+    );
 }
