ecd5ef211e3f7eb8ef72684a8dab8a8e8ab34d43
ajay-bhagava <ajay.bhagava@gmail.com>
2025-01-18 14:23:28 -0500

    completed login

---
+++ app/login/layout.tsx
@@ -0,0 +1,8 @@
+export default function layout({ children }: any) {
+    return <div className="h-screen">
+        <nav className="flex align-center flex-row p-4">
+            <a href="/" className="link">Home</a>
+        </nav>
+        {children}
+    </div>
+}
