a1bf4e93ea3baa95f1b1ccc69f561733d0f6e644
Christopher Lam <127339027+clam365@users.noreply.github.com>
2025-01-18 20:22:01 -0500

    ytdydyrd

--- app/mylistings/page.tsx
+++ app/mylistings/page.tsx
@@ -1,24 +1,12 @@
 import Image from "next/image";
 import { CircleUserRound, DiamondPlus, MessageCircle } from "lucide-react";
 import Content from "@/app/mylistings/Content";
+import NavBar from "@/components/navbar";
 
 export default function page() {
     return (
         <>
-            <div className={"p-3 flex justify-between items-center drop-shadow-md bg-white"}>
-                <div className="flex items-center gap-2">
-                    <Image src={"/techtrade.png"} alt={"techtrade"} width={45} height={45} className={""} />
-                    <div className="text-2xl font-bold flex items-center">
-                        <span className="text-red-600">Tech</span>
-                        <span className="text-black">Trade</span>
-                    </div>
-                </div>
-                <div className={"gap-x-3 flex"}>
-                    <DiamondPlus className={"w-9 h-9"} />
-                    <MessageCircle className={"w-9 h-9"} />
-                    <CircleUserRound className={"w-9 h-9"} />
-                </div>
-            </div>
+            <NavBar/>
             <Content />
         </>
     );
