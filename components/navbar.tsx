ef1fd32fcbe82bc0c6bde4c4634ff85619030ea9
jeremymkurtz <kurtzjeremy126@gmail.com>
2025-01-18 22:07:12 -0500

    change profile to info icon and about page route

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import {CircleUserRound, DiamondPlus, MessageCircle} from "lucide-react";
+import {Info, DiamondPlus, MessageCircle} from "lucide-react";
 import Link from "next/link";
 
 export default function NavBar() {
-    return <div className={"p-3 flex justify-between items-center drop-shadow-md bg-white"}>
+    return <div className={"p-3 flex justify-between items-center drop-shadow-md bg-white z-30"}>
         <Link href={"/marketplace"}>
             <div className="flex items-center gap-2">
                 <Image src={"/techtrade.png"} alt={"techtrade"} width={45} height={45} className={""}/>
@@ -16,7 +16,7 @@ export default function NavBar() {
         <div className={"gap-x-3 flex"}>
             <Link href={"/post"}><DiamondPlus className={"w-9 h-9"}/></Link>
             <MessageCircle className={"w-9 h-9"}/>
-            <CircleUserRound className={"w-9 h-9"}/>
+            <Link href={"/about"}><Info className={"w-9 h-9"}/></Link>
         </div>
     </div>
 }
